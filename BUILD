ff414018d6721cf461c6901bded4fa3cf7c6de50
Evan Pratten <ewpratten@gmail.com>
2020-02-10 13:36:09 -0500

    add gradle

--- BUILD
+++
@@ -1,6 +0,0 @@
-load("@rules_java//java:defs.bzl", "java_binary")
-
-java_binary(
-    name = "pointmesh",
-    srcs = glob(["src/main/java/ca/retrylife/ics4u/pointmesh/**.java"]),
-)
---
+++ src/main/java/ca/retrylife/ics4u/pointmesh/App.java
@@ -0,0 +1,17 @@
+package ca.retrylife.ics4u.pointmesh;
+
+public class App implements Runnable {
+
+    public static void main(String[] args) {
+    }
+
+    private App() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+}
